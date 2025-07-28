9f1c1c4f257d8c0ae78c77d65dc99ab935357928
Sanika <sanikag6307@gmail.com>
2025-07-28 23:32:35 +0530

    initial commit.

---
+++ asssignment5/src/asssignment5/Account.java
@@ -0,0 +1,61 @@
+package asssignment5;
+
+abstract class Account {
+    protected String accountHolder;
+    protected double balance;
+
+    public Account(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+    public abstract double getBalance();
+
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: ₹" + getBalance());
+    }
+}
+
+class SavingsAccount extends Account {
+    public SavingsAccount(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance in Savings Account.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+class CheckingAccount extends Account {
+    public CheckingAccount(String accountHolder, double balance) {
+        super(accountHolder, balance);
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
