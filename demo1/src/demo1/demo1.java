83fd7abc4ada4418bc637c92f04b7c609f58141b
Sanika <sanikag6307@gmail.com>
2025-07-28 23:10:36 +0530

    initial commit.

---
+++ demo1/src/demo1/demo1.java
@@ -0,0 +1,14 @@
+package demo1;
+
+
+public class demo1 {
+	public static void main(String[] args) {
+		int age=15;
+		if(age>=18) {
+			System.out.println("Person is Eligible for voting");
+		}else {
+			System.out.println("Perosn is not eligible for voting");
+		}
+	}
+
+}
